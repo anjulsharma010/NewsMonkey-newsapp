d8d9f6d35973980b948bf0532e2949d94734f6f4
Anjul <anjulsharma010@gmail.com>
2023-06-29 17:12:25 +0530

    third commit

--- src/components/News.js
+++ src/components/News.js
@@ -1,56 +1,81 @@
 import React, { Component } from "react";
 import NewsItem from "./NewsItem";
+import Spinner from "./Spinner";
+import PropTypes from 'prop-types'
+
 
 export class News extends Component {
-  articles =  [
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      "description": "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      "publishedAt": "2020-04-27T11:41:47Z",
-      "content": "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]"
-    },
-    {
-      "source": { "id": "espn-cric-info", "name": "ESPN Cric Info" },
-      "author": null,
-      "title": "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      "description": "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      "url": "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      "urlToImage": "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      "publishedAt": "2020-03-30T15:26:05Z",
-      "content": "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]"
-    }
-  ]
+  static defaultProps = {
+    country: 'in',
+    pageSize: 8,
+    category: 'general'
+
+  }
+
+  static propTypes = {
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
   constructor(){
     super();
     this.state ={
-      articles: this.articles,
-      loading: false
+      articles: [],
+      loading: false,
+      page: 1,
+      
     }
   }
 
   async componentDidMount(){
-    let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=aaaf23f501894ebd8787d0c87f77183b"
+    let url =`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=aaaf23f501894ebd8787d0c87f77183b&page=1&pagesize=${this.props.pageSize}`
+    this.setState({loading: true});
     let data = await fetch(url);
     let parsedData = await data.json()
-    this.setState({articles: parsedData.articles})
+    this.setState({articles: parsedData.articles, totalResults:  parsedData.totalResults, loading:false})
   }
-
+  handlePreviousClick =async ()=>{
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=aaaf23f501894ebd8787d0c87f77183b&page=${this.state.page - 1}&pagesize=${this.props.pageSize}`
+    this.setState({loading: true});
+    let data = await fetch(url);
+    let parsedData = await data.json()
+   this.setState({
+      page: this.state.page - 1,
+      articles: parsedData.articles,
+      loading: false
+    })
+  }
+   handleNextClick = async()=>{
+    if(!(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
+     this.setState({loading: true});
+    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=aaaf23f501894ebd8787d0c87f77183b&page=${this.state.page + 1}&pagesize=${this.props.pageSize}`
+    let data = await fetch(url);
+    let parsedData = await data.json()
+   this.setState({
+      page: this.state.page + 1,
+      articles: parsedData.articles,
+      loading: false
+    })
+    }
+  }
   render() {
     return (
       <div className="container">
         <div className="container my-3">
         <div className="row">
-          <h2>NewsMonkey - Top Headlines</h2>
-          {this.state.articles.map((element)=>{
+          <h1 className="text-center"style={{margin: '35px 0px'}}>NewsMonkey - Top Headlines</h1>
+          {this.state.loading && <Spinner/>}
+          {!this.state.loading && this.state.articles.map((element)=>{
            return <div className="col-md-4" key={element.url} >
-              <NewsItem title={element.title ? element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 88):""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+              <NewsItem title={element.title ? element.title.slice(0, 45):""} description={element.description?element.description.slice(0, 88):""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name} />
             </div>
          })}
           </div>
+        </div>
+        <div className="container d-flex justify-content-between">
+        <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePreviousClick}> &larr; Previous</button>
+        <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr; </button>
         </div>
       </div>
     );
@@ -59,22 +84,4 @@ export class News extends Component {
 
 export default News;
 
-{
-  /* <div clasName="container my-3">
-        <h2>NewsMonkey - Top Headlines</h2>
-          <div clasName="row">
-          <div className="col-sm">
-              <NewsItem title="myTitle" description="mydesc"/>
-            </div>
-            <div className="col-sm">
-              <NewsItem title="myTitle" description="mydesc"/>
-            </div>
-            <div className="col-sm">
-              <NewsItem title="myTitle" description="mydesc"/>
-            </div>
-            <div className="col-sm">
-              <NewsItem title="myTitle" description="mydesc"/>
-            </div>
-          </div>
-      </div> */
-}
+
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,22 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
-  
   render() {
-   let {title, description, imageUrl, newsUrl} = this.props;
+    let { title, description, imageUrl, newsUrl, author, date,source } = this.props;
     return (
       <div className="my-3">
-       <div className="card" style={{width: "18rem"}}>
-  <img src={!imageUrl?"https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=600":imageUrl} className="card-img-top" alt="..."/>
-  <div className="card-body">
-    <h5 className="card-title">{title}...</h5>
-  <p className="card-text">{description}...</p>
-    <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">Read More</a>
-  </div>
-</div>
+        <div className="card">
+          <img
+            src={
+              !imageUrl
+                ? "https://images.pexels.com/photos/39284/macbook-apple-imac-computer-39284.jpeg?auto=compress&cs=tinysrgb&w=600"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+          <span class="position-absolute top-0  translate-middle badge rounded-pill bg-danger " style={{left: '90%', zIndex: '1'}}>
+                {source}
+              </span>
+            <h5 className="card-title">
+              {title}...
+             </h5>
+            <p className="card-text">{description}...</p>
+            <p class="card-text">
+              <small className="text-body-secondary">
+                By {!author ? "Unknown" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn btn-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
